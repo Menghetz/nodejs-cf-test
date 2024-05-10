5e0591adcc61173e32a0a42ce1b8dbb35b3aab75
MENGHETTI <matteo.menghetti@solvay.com>
2024-05-10 17:58:41 +0200

    changed folder structure

--- index.js
+++
@@ -1,5 +0,0 @@
-const functions = require('@google-cloud/functions-framework');
-
-exports.helloHttp = functions.http('helloHttp', (req, res) => {
-    res.send(`Hello ${req.query.name || req.body.name || 'World'}!`);
-});
